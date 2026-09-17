c94d225876f25188eda60b111b458073fabf6236
Noemy Rivera <noemy96@live.cl>
2026-09-17 14:34:18 -0300

    act

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react-swc"
-import path from "path"
-import { componentTagger } from "lovable-tagger"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   preview: {
-    allowedHosts: ["mediaciondiegodealmagro.cl"], // ✅ ← esta línea permite tu dominio
+    allowedHosts: ["mediaciondiegodealmagro.cl"],
   },
   plugins: [
     react(),
@@ -21,4 +21,4 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}))
+}));
